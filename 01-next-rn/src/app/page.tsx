441dc263f69740f944ecbf6775ac64ca6d38df83
The Hieu <thehieu236@gmail.com>
2024-09-20 00:04:18 +0700

    setup

---
+++ 01-next-rn/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd/es/radio";
+
+
+export default function Home() {
+  return (
+    <div>
+      Hello word
+      <Button title="Add new"></Button>
+    </div>
+  );
+}
---
+++ 02-nest-rn/src/users/users.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { application } from 'express';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
